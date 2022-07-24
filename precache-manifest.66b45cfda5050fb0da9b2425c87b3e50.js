f152643a8cd7b19cd783c868d8df524d1c46900b
Naveen <ns4397569@gmail.com>
2022-07-24 14:44:04 +0530

    Updates

---
+++ precache-manifest.66b45cfda5050fb0da9b2425c87b3e50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5ce7e2bf8879db046d7",
+    "url": "/Text-Analyzer/static/css/main.26bc1ebb.chunk.css"
+  },
+  {
+    "revision": "d5ce7e2bf8879db046d7",
+    "url": "/Text-Analyzer/static/js/main.d5ce7e2b.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Text-Analyzer/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/Text-Analyzer/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "b64e9556360d0f23a23b",
+    "url": "/Text-Analyzer/static/js/runtime~main.b64e9556.js"
+  },
+  {
+    "revision": "43d866b451c3d8f5335949fd4f3157fd",
+    "url": "/Text-Analyzer/index.html"
+  }
+];
